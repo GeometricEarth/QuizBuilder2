baa61cd4b8db64dc56657fc8cf6eac70e46c3242
Geometr <geo.fsv.p@gmail.com>
2018-05-25 22:57:00 +0300

    1st

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,18 +7,20 @@ import styles from "./app.css";
 export default class App {
     constructor({
         conteiner
-    }){
-        
+    }) {
+
 
         this.conteiner = conteiner;
         this.elem = document.createElement('div');
         this.elem.classList = "app";
         this.conteiner.append(this.elem);
+
+        this.list = new Question__list({
+            conteiner: this.elem,
+        });
         
         this.field = new Field({
             conteiner: this.elem,
         });
-
-        this.list = new Question__list(this.elem);
     }
 }
--- src/components/question__list/question__list.js
+++ src/components/question__list/question__list.js
@@ -1,5 +1,13 @@
 import style from "./question__list.css";
 
 export default class Question__list {
-    
+    constructor({
+        conteiner
+    }) {
+        this.conteiner = conteiner;
+        this.elem = document.createElement('div');
+        this.elem.classList = "list";
+        this.conteiner.append(this.elem);
+    }
+
 }
