cc19a2cf510c7f091b8b925d16c7e4acf7df6caa
Geometr <geo.fsv.p@gmail.com>
2018-06-06 01:31:11 +0300

    извлечение данных формы

--- src/components/field/field.js
+++ src/components/field/field.js
@@ -19,7 +19,26 @@ export default class Field {
     save_Question(event){
         event.preventDefault();
         let form = event.target;
-        console.dir(event);
+        let radio_btn = Array.from(form.true);
+        let inputs = form.inp;
+        let data = {
+            question:form.question.value,
+            true_answer:'',
+            answers: [],
+        };
+        radio_btn.forEach(
+            (element, id , arr)=>{
+                if(element.checked){
+                    data.true_answer = element.value;
+                }
+            }
+        )
+
+        inputs.forEach(element => {
+            data.answers.push(element.value);
+        });
+        
+        console.dir(data);
     }
 
     template(){
@@ -34,31 +53,31 @@ export default class Field {
                     
                     <div>
                         <div class="roundedOne">
-                            <input type="radio" id="roundedOne1" name="true"/>
+                            <input type="radio" id="roundedOne1" name="true" value="1"/>
                             <label for="roundedOne1"></label>
                         </div>
-                        <input name="inp1" class="field_answer" id="inp1" placeholder="Вариант ответа 1" type="text">
+                        <input name="inp" class="field_answer" id="inp1" placeholder="Вариант ответа 1" type="text">
                     </div>    
                     <div>  
                         <div class="roundedOne">
-                            <input type="radio" id="roundedOne2" name="true"/>
+                            <input type="radio" id="roundedOne2" name="true" value="2"/>
                             <label for="roundedOne2"></label>
                         </div>
-                        <input name="inp2" class="field_answer" id="inp2" placeholder="Вариант ответа 2" type="text">
+                        <input name="inp" class="field_answer" id="inp2" placeholder="Вариант ответа 2" type="text">
                     </div>
                     <div>
                         <div class="roundedOne">
-                            <input type="radio" value="None" id="roundedOne3" name="true"/>
+                            <input type="radio" id="roundedOne3" name="true" value="3"/>
                             <label for="roundedOne3"></label>
                         </div>
-                        <input name="inp3" class="field_answer" id="inp3" placeholder="Вариант ответа 3" type="text">
+                        <input name="inp" class="field_answer" id="inp3" placeholder="Вариант ответа 3" type="text">
                     </div>
                     <div>
                         <div class="roundedOne">
-                            <input type="radio" value="None" id="roundedOne4" name="true"/>
+                            <input type="radio" id="roundedOne4" name="true" value="4"/>
                             <label for="roundedOne4"></label>
                         </div>
-                        <input name="inp4" class="field_answer" id="inp4" placeholder="Вариант ответа 4" type="text">
+                        <input name="inp" class="field_answer" id="inp4" placeholder="Вариант ответа 4" type="text">
                     </div>
                     <div class="field_control">
                     <input type="submit" class="field_submit field_button" value="Отправить">
