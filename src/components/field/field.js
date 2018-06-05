3df659c55f0fd335257bf731214027bf8e38b2d8
Geometr <geo.fsv.p@gmail.com>
2018-06-05 18:00:13 +0300

    стилизация формы

--- src/components/field/field.js
+++ src/components/field/field.js
@@ -18,38 +18,51 @@ export default class Field {
 
     save_Question(event){
         event.preventDefault();
+        let form = event.target;
+        console.dir(event);
     }
 
     template(){
         return `
-                    <h2>Добавить вопрос</h2>
-                    <img id="load_icon" onclick="OpenPic()" src="..\images\no-image-icon.png">
+                    <div class="field_header">
+                        <h2>Добавить вопрос</h2>
+                    </div>
+                    <img class="field_load_img" src="./src/assets/no-image-icon.png">
                     
-                       <textarea id="questionTextArea" name="limitedtextarea" placeholder="Текст вопроса" onKeyDown="limitText(this,120);" onKeyUp="limitText(this,120);"
+                    <textarea id="questionTextArea" name="question" placeholder="Текст вопроса"
                     rows="4"></textarea>
-                    <div class="roundedOne">
-      <input type="checkbox" value="None" id="roundedOne" name="check" checked />
-      <label for="roundedOne"></label>
-    </div>
+                    
                     <div>
-                        <div class="roundedOne"><input name="right" class="roundedOne" type="radio"></div>
+                        <div class="roundedOne">
+                            <input type="radio" id="roundedOne1" name="true"/>
+                            <label for="roundedOne"></label>
+                        </div>
                         <input name="inp1" class="field_answer" id="inp1" placeholder="Вариант ответа 1" type="text">
                     </div>    
                     <div>  
-                        <input name="right" type="radio">
+                        <div class="roundedOne">
+                            <input type="radio" id="roundedOne2" name="true"/>
+                            <label for="roundedOne2"></label>
+                        </div>
                         <input name="inp2" class="field_answer" id="inp2" placeholder="Вариант ответа 2" type="text">
                     </div>
                     <div>
-                        <input name="right" type="radio">
+                        <div class="roundedOne">
+                            <input type="radio" value="None" id="roundedOne3" name="true"/>
+                            <label for="roundedOne3"></label>
+                        </div>
                         <input name="inp3" class="field_answer" id="inp3" placeholder="Вариант ответа 3" type="text">
                     </div>
                     <div>
-                        <input name="right" type="radio">
+                        <div class="roundedOne">
+                            <input type="radio" value="None" id="roundedOne4" name="true"/>
+                            <label for="roundedOne4"></label>
+                        </div>
                         <input name="inp4" class="field_answer" id="inp4" placeholder="Вариант ответа 4" type="text">
                     </div>
                     <div class="field_control">
-                    <input type="submit" class="field_submit" value="Отправить">
-                    <button>Очистить</button>
+                    <input type="submit" class="field_submit field_button value="Отправить">
+                    <button type="clear" class="field_button">Очистить</button>
                     </div>
         `
     }
