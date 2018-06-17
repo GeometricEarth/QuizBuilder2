331700ec6c21583e5c20c41d329f146c80ef2cc0
Geometr <geo.fsv.p@gmail.com>
2018-06-17 16:35:03 +0300

    теперь только данные

--- mocks/question_mock.js
+++ mocks/question_mock.js
@@ -1,36 +1,15 @@
-import style from "../src/components/question/question";
+// import style from "../src/components/question/question";
 
-function template (){
-    return `
-    <div class="question" id="0">
-        <button class="question-accordion active">
-            <span class="question-num">1</span>
-            <span class="question-button">Текст вопроса</span>
-        </button>
-        <div class="question-panel" style="max-height: 490px;">
-            <img class="question-icon" src="">
-            <p>
-                <span class="question-answer-num question-rightAnswer">1</span>
-                <span class="question-answer">Ответ</span>
-            </p>
-            <p>
-                <span class="question-answer-num">2</span>
-                <span class="question-answer">Ответ</span>
-            </p>
-            <p>
-                <span class="question-answer-num">3</span>
-                <span class="question-answer">Ответ</span>
-            </p>
-            <p>
-                <span class="question-answer-num">4</span>
-                <span class="question-answer">Ответ</span>
-                </p>
-            <div class="question-control">
-                <button class="field_button">Редактировать</button>
-            </div>
-        </div>
-    </div>
-    `
-};
+let data = {
+    question:'Текст вопроса',
+    true_answer: 1,
+    answers: [
+        'Вариант один',
+        'Вариант два',
+        'Вариант три',
+        'Вариант четыре'
+    ],
+    img: './src/assets/no-image-icon.png',
+}
 
-export default template;
+export default data;
