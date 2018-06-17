23b2de33df3e9bdf8d32925f0a61c04c0070d621
Geometr <geo.fsv.p@gmail.com>
2018-06-17 16:50:24 +0300

    + базовая реализация добавления question и поведение акардиона

--- src/components/question__list/question__list.js
+++ src/components/question__list/question__list.js
@@ -1,5 +1,5 @@
 import style from "./question__list.css";
-import Question from "../../../mocks/question_mock";
+import Question from "../question/question";
 
 export default class Question__list {
     constructor({
@@ -9,10 +9,30 @@ export default class Question__list {
         this.elem = document.createElement('div');
         this.elem.classList = "list";
         this.conteiner.append(this.elem);
+        this.questions__storage = [];
+
+        this.elem.addEventListener(
+            'click',
+            this.expand.bind(this)
+        )
+    }
+
+    expand(event) {
+        let target = event.target.closest(".question-accordion");
+        target.classList.toggle("active");
+        let panel = target.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
     }
 
     addQuestion(data) {
-        this.elem.innerHTML += Question()
-    }
+        this.questions__storage.push(data);
+        data.id = this.questions__storage.length;
+        new Question({conteiner:this.elem, data});
+        console.dir(this.questions__storage)
+    } 
 
 }
